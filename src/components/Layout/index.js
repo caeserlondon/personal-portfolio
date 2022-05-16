1cd0af64c97536734cbb2ed26897a2d81344dd72
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-16 16:19:00 +0700

    Creating the Sidebar component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../Sidebar'
+import './index.scss'
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
+
+export default Layout
