c184060d1f5b182ecd0211b91ab7a41bd45b0993
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-18 21:55:04 +0700

    Adding the Home component to the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.scss'
+import Home from './components/HOME'
 import Layout from './components/Layout'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   )
