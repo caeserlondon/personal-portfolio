05cd2f02ca2b0db1c5bef32f8c9d20115c37f7d7
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-19 00:50:53 +0700

    Adding content to the Home Component

--- src/components/HOME/index.js
+++ src/components/HOME/index.js
@@ -1,3 +1,25 @@
-const Home = () => 'HOME'
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          aeser
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend / Backend / Graphic Design</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
 
 export default Home
