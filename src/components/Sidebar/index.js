08ffe66d757f4a3a06e3fa7bb1c8ab6b66aa7c2f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-18 21:41:34 +0700

    Adding the Outlet and the Tags

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -8,6 +8,10 @@ import { AiOutlineHome } from 'react-icons/ai'
 import { FiUser } from 'react-icons/fi'
 import { FaRegEnvelope } from 'react-icons/fa'
 
+import { AiOutlineLinkedin } from 'react-icons/ai'
+import { VscGithub } from 'react-icons/vsc'
+import { SiCodewars } from 'react-icons/si'
+
 const Sidebar = () => {
   return (
     <div className="nav-bar">
@@ -23,7 +27,7 @@ const Sidebar = () => {
           exact="true"
           activeclassname="active"
           className="about-link"
-          to="/"
+          to="/about-link"
         >
           <FiUser />
         </NavLink>
@@ -31,11 +35,40 @@ const Sidebar = () => {
           exact="true"
           activeclassname="active"
           className="contact-link"
-          to="/"
+          to="/contact"
         >
           <FaRegEnvelope />
         </NavLink>
       </nav>
+      <ul>
+        <li>
+          <a
+            target="_blanck"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/caeser-ibrahim-3bb4a322a/"
+          >
+            <AiOutlineLinkedin />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blanck"
+            rel="noreferrer"
+            href="https://github.com/caeserlondon"
+          >
+            <VscGithub />
+          </a>
+        </li>{' '}
+        <li>
+          <a
+            target="_blanck"
+            rel="noreferrer"
+            href="https://www.codewars.com/users/Caeser%20Ibrahim"
+          >
+            <SiCodewars />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
