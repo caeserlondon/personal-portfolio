1604f908a92cbd2c257e0efa2a9b0ca26328ace6
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-20 20:47:02 +0100

    Creating the AnimatedLetters Component

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.scss'
+
+const AnimatedLetters = ({ letterClass, strArray, index }) => {
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass}} _${i + index}`}>
+          {char}
+        </span>
+      ))}
+    </span>
+  )
+}
+
+export default AnimatedLetters
