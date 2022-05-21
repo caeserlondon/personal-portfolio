12c569c311478bc4a822da61d932d6c716c0af10
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-21 19:45:34 +0100

    Adding the Logo component inside the Home component

--- src/components/HOME/index.js
+++ src/components/HOME/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import Logo from './Logo'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -58,6 +59,7 @@ const Home = () => {
           CONTACT ME
         </Link>
       </div>
+      <Logo />
     </div>
   )
 }
