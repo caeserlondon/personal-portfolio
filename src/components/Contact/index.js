f521497bafaaba73823580b1ea58f6789300c938
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-22 11:20:21 +0100

    Adding the animated letters component and the loader to the contact page component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Contact = () => {
-  return <div>Contact</div>
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              index={15}
+            />
+          </h1>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Contact
