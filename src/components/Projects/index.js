472274926edf5e1234b3f769ce9b303bcd49d143
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-24 20:39:28 +0100

    Adding the spiral animation

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,292 +2,64 @@ import React, { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import IMG1 from '../../assets/gallery/marble.jpeg'
+import Data from '../../Data'
 
 const Projects = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const projArray = [
-    'M',
-    'y',
-    ' ',
-    'P',
-    'r',
-    'o',
-    'j',
-    'e',
-    'c',
-    't',
-    's',
-    ' ',
-    ':',
-  ]
+  const projArray = ['Some', ' ', 'Off', ' ', 'My', ' ', 'Projects', ' ', ':']
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
-    }, 3000)
+    }, 2000)
   }, [])
 
   return (
     <>
-      {/* <div className="projects-page"> */}
       <div className="text-zone">
         <h1>
           <AnimatedLetters
             letterClass={letterClass}
             strArray={projArray}
-            index={15}
+            index={5}
           />
         </h1>
       </div>
       <div className="portfolio-container">
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item-image">
-            <img src={IMG1} alt="project-1-name" />
-          </div>
-          <h3>
-            Project name
-            <span> project description and the technologies used</span>
-          </h3>
-          <div className="pro-buttons">
-            <a
-              href="http://netlify.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Live Demo
-            </a>
-            <a
-              href="http://github.com"
-              target="_blank"
-              rel="noreferrer"
-              className="flat-button"
-            >
-              Github
-            </a>
-          </div>
-        </article>
+        {Data.map(({ id, image, title, description, github, demo }) => {
+          return (
+            <article key={id} className="portfolio-item">
+              <div className="portfolio-item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>
+                {title}
+                <span>{description}</span>
+              </h3>
+              <div className="pro-buttons">
+                <a
+                  href={demo}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flat-button"
+                >
+                  Live Demo
+                </a>
+                <a
+                  href={github}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flat-button"
+                >
+                  Github
+                </a>
+              </div>
+            </article>
+          )
+        })}
       </div>
       <Loader type="ball-scale" />
-      {/* </div> */}
     </>
   )
 }
