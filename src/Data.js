2a54de8ce1bc2368c256b892b7a62cab1c902841
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-24 20:41:19 +0100

    Adding the projects place holder data

---
+++ src/Data.js
@@ -0,0 +1,78 @@
+import IMG1 from './assets/gallery/marble.jpeg'
+
+const Data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+  {
+    id: 6,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+  {
+    id: 7,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+  {
+    id: 8,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+  {
+    id: 9,
+    image: IMG1,
+    title: 'Project title: ',
+    description: 'project description and the technologies used',
+    github: 'https://github.com',
+    demo: 'https://netlify.com',
+  },
+]
+
+export default Data
