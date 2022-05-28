882c0d798c042a15b24c2a9340a85faca4e0c300
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-28 23:08:01 +0100

    Updating the letter inside the envelop

--- src/components/CVEnvelop/index.js
+++ src/components/CVEnvelop/index.js
@@ -3,24 +3,26 @@ import './index.scss'
 
 const Envelop = () => {
   return (
-    <div class="letter-image">
-      <div class="animated-mail">
-        <div class="back-fold"></div>
-        <div class="letter">
-          <div class="letter-border"></div>
-          <div class="letter-title"></div>
-          <div class="letter-context">
+    <div className="letter-image">
+      <div className="animated-mail">
+        <div className="back-fold"></div>
+        <div className="letter">
+          <div className="letter-border"></div>
+          <div className="letter-title"></div>
+          <div className="letter-context">
             <p>DOWNLOAD MY C.V.</p>
           </div>
-          <div class="letter-stamp">
-            <div class="letter-stamp-inner"></div>
+          <div className="letter-stamp">
+            <div className="letter-stamp-inner">
+              <p>Caeser</p>
+            </div>
           </div>
         </div>
-        <div class="top-fold"></div>
-        <div class="body"></div>
-        <div class="left-fold"></div>
+        <div className="top-fold"></div>
+        <div className="body"></div>
+        <div className="left-fold"></div>
       </div>
-      <div class="shadow"></div>
+      <div className="shadow"></div>
     </div>
   )
 }
