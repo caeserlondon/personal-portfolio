6ab2422e015435d0dc6d78230fadbf1b47be28a0
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-28 23:04:27 +0100

    Updating the projects number

--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,6 @@
 import IMG1 from './assets/gallery/marble.jpeg'
 
+// --- 6 projects
 const Data = [
   {
     id: 1,
@@ -49,30 +50,81 @@ const Data = [
     github: 'https://github.com',
     demo: 'https://netlify.com',
   },
-  {
-    id: 7,
-    image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
-  },
-  {
-    id: 8,
-    image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
-  },
-  {
-    id: 9,
-    image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
-  },
 ]
+///   9 projects ---
+// const Data = [
+//   {
+//     id: 1,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+//   {
+//     id: 2,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+//   {
+//     id: 3,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+//   {
+//     id: 4,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+//   {
+//     id: 5,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+//   {
+//     id: 6,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+//   {
+//     id: 7,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+//   {
+//     id: 8,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+//   {
+//     id: 9,
+//     image: IMG1,
+//     title: 'Project title: ',
+//     description: 'project description and the technologies used',
+//     github: 'https://github.com',
+//     demo: 'https://netlify.com',
+//   },
+// ]
 
 export default Data
