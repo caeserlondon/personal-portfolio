f6599396f1876f5e9871efe002845e3d25c95841
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-04 18:44:10 +0100

    Adding the Error page and it's Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss'
 import About from './components/About'
 import Contact from './components/Contact'
 import Home from './components/HOME'
+import Error from './components/Error'
 import Layout from './components/Layout'
 import Projects from './components/Projects'
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<Error />} />
         </Route>
       </Routes>
     </>
