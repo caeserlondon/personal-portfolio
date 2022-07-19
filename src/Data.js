8af705326f2c19901de01f7fffb48b7affc8dd35
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-19 19:43:12 +0100

    Updating the projects data

--- src/Data.js
+++ src/Data.js
@@ -1,55 +1,74 @@
-import IMG1 from './assets/gallery/marble.jpeg'
+import IMG1 from './assets/gallery/yum-yum.jpg'
+import IMG2 from './assets/gallery/london-home-finder.jpg'
+import IMG3 from './assets/gallery/vet-clinic.jpg'
+import IMG4 from './assets/gallery/github-finder.jpg'
+import IMG5 from './assets/gallery/car-dealership.jpg'
+import IMG6 from './assets/gallery/color-picker.jpg'
 
 // --- 6 projects
 const Data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
+    title: 'Yum Yum: ',
+    description:
+      'Recipe Website using Spoonacular API, With Multiple Themes, Made using React 18, React Icons and SASS.',
+    github: 'https://github.com/caeserlondon/Yum-Yum',
+    demo: 'https://yum-yum-caeserlondon.vercel.app',
   },
   {
     id: 2,
-    image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
+    image: IMG2,
+    title: 'London Home Finder: ',
+    description:
+      'For listings properties for rent and sale. using React, firebase, googleAuth, and leaflet-maps.',
+    github: 'https://github.com/caeserlondon/london-home-finder',
+    demo: 'https://london-home-finder.web.app/',
   },
   {
     id: 3,
-    image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
+    image: IMG3,
+    title: 'Vet Clinic: ',
+    description:
+      'Building a Veterinary clinic web site with Next V12.2, React V18.2, photos and videos pexels.com',
+    github: 'https://github.com/caeserlondon/vet-clinic',
+    demo: 'https://caeser-vet-clinic.vercel.app',
   },
   {
     id: 4,
-    image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
+    image: IMG4,
+    title: 'Github Finder: ',
+    description:
+      'Search and find Github users and view their profile, created with React, TailWind and Daisy UI.',
+    github: 'https://github.com/caeserlondon/github-user-finder',
+    demo: 'https://github-user-finder-caeser.vercel.app/',
   },
   {
     id: 5,
-    image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
+    image: IMG5,
+    title: 'Car Dealership: ',
+    description:
+      'Building a fully responsive Classic Car Dealer website, using HTML, CSS, jQuery and font awesome.',
+    github: 'https://github.com/caeserlondon/car-dealership',
+    demo: 'https://caesers-classic-cars.netlify.app',
   },
   {
     id: 6,
-    image: IMG1,
-    title: 'Project title: ',
-    description: 'project description and the technologies used',
-    github: 'https://github.com',
-    demo: 'https://netlify.com',
+    image: IMG6,
+    title: 'Colo Picker: ',
+    description:
+      'Create a random color palette.Created using JavaScript, Chroma.js, HTML5, CSS3 and font awesome.',
+    github: 'https://github.com/caeserlondon/Color-Picker',
+    demo: 'https://caeser-color-picker.netlify.app/',
   },
+  // {
+  //   id: 6,
+  //   image: IMG1,
+  //   title: 'Project title: ',
+  //   description: 'project description and the technologies used',
+  //   github: 'https://github.com',
+  //   demo: 'https://netlify.com',
+  // },
 ]
 ///   9 projects ---
 // const Data = [
